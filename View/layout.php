0185ac07893fd6fa6930e6a7e287fcea9d90b0e7
Li_Lv99 <lleiva10893@ufide.ac.cr>
2025-12-05 23:39:07 -0600

    Update navigation links and resource paths

--- View/layout.php
+++ View/layout.php
@@ -11,31 +11,35 @@ function MostrarMenu() {
     echo '
     <nav class="navbar navbar-expand-lg navbar-dark bg-blue-dark">
         <div class="container-fluid px-5 d-flex justify-content-between align-items-center">
-            <a class="navbar-brand" href="/OptiGestion/index.php">Óptica Grisol</a>
+            <a class="navbar-brand" href="/index.php">Óptica Grisol</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" href="/OptiGestion/index.php">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/OptiGestion/view/about.php">Sobre Nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/OptiGestion/view/anteojos.php">Anteojos</a></li>';
+
+                    <li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/View/about.php">Sobre Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/View/anteojos.php">Anteojos</a></li>';
 
     if (!$rol) {
-        echo '<li class="nav-item ms-lg-3"><a class="nav-link" href="/OptiGestion/view/iniciarSesion.php">Iniciar Sesión</a></li>';
-    } else if ($rol === 'Paciente') {
+        echo '<li class="nav-item ms-lg-3"><a class="nav-link" href="/View/iniciarSesion.php">Iniciar Sesión</a></li>';
+    } 
+    else if ($rol === 'Paciente') {
         echo '
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCitas" role="button" data-bs-toggle="dropdown">
                 Citas
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownCitas">
-                <li><a class="dropdown-item" href="/OptiGestion/view/appointmentForm.php">Agendar Cita</a></li>
-                <li><a class="dropdown-item" href="/OptiGestion/view/editarcita.php">Mis Citas</a></li>
-                <li><a class="dropdown-item" href="/OptiGestion/view/historialMedico.php">Historial Médico</a></li>
+                <li><a class="dropdown-item" href="/View/appointmentForm.php">Agendar Cita</a></li>
+                <li><a class="dropdown-item" href="/View/editarcita.php">Mis Citas</a></li>
+                <li><a class="dropdown-item" href="/View/historialMedico.php">Historial Médico</a></li>
             </ul>
         </li>';
-    } else if ($rol === 'Empleado') {
+    } 
+    else if ($rol === 'Empleado') {
+
         if ($EmpleadoRol == 1) {
             echo ' 
             <li class="nav-item dropdown">
@@ -43,7 +47,7 @@ function MostrarMenu() {
                     Personal
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPersonal">
-                    <li><a class="dropdown-item" href="/OptiGestion/view/personal.php">Ver Personal</a></li>
+                    <li><a class="dropdown-item" href="/View/personal.php">Ver Personal</a></li>
                 </ul>
             </li>';
         }
@@ -54,12 +58,12 @@ function MostrarMenu() {
                 Administración
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownAdmin">
-                <li><a class="dropdown-item" href="/OptiGestion/view/reportes.php">Reportes</a></li>
-                <li><a class="dropdown-item" href="/OptiGestion/view/inventario.php">Inventario</a></li>
-                <li><a class="dropdown-item" href="/OptiGestion/view/facturacion.php">Facturación</a></li>
-                <li><a class="dropdown-item" href="/OptiGestion/view/historialExpedientes.php">Historial de Expedientes</a></li>
-               <li><a class="dropdown-item" href="/OptiGestion/view/editarcita.php">Manipular Citas</a></li>
-                </ul>
+                <li><a class="dropdown-item" href="/View/reportes.php">Reportes</a></li>
+                <li><a class="dropdown-item" href="/View/inventario.php">Inventario</a></li>
+                <li><a class="dropdown-item" href="/View/facturacion.php">Facturación</a></li>
+                <li><a class="dropdown-item" href="/View/historialExpedientes.php">Historial de Expedientes</a></li>
+                <li><a class="dropdown-item" href="/View/editarcita.php">Manipular Citas</a></li>
+            </ul>
         </li>';
     }
 
@@ -70,9 +74,8 @@ function MostrarMenu() {
                 Perfil
             </a>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownPerfil">
-                <li><a class="dropdown-item" href="/OptiGestion/view/editarPerfil.php">Editar Perfil</a></li>
-                
-                <li><a class="dropdown-item" href="/OptiGestion/logout.php">Cerrar Sesión</a></li>
+                <li><a class="dropdown-item" href="/View/editarPerfil.php">Editar Perfil</a></li>
+                <li><a class="dropdown-item" href="/logout.php">Cerrar Sesión</a></li>
             </ul>
         </li>';
     }
@@ -87,50 +90,50 @@ function MostrarMenu() {
 function MostrarFooter() {
     echo '
 <footer class="bg-dark text-light pt-3 pb-2 mt-auto">
-        <div class="container text-center">
-            <div class="row justify-content-center">
-                <div class="col-md-3 mb-2">
-                    <h6 class="fw-bold text-uppercase">Sucursal</h6>
-                    <a href="https://maps.app.goo.gl/8xCe7rQRBhBzRZsr7" class="text-light text-decoration-none small">Ver ubicación</a>
-                </div>
-                <div class="col-md-3 mb-2">
-                    <h6 class="fw-bold text-uppercase">Redes Sociales</h6>
-                    <p class="mb-1 small">
-                        <a href="https://www.instagram.com/opticagrisol?igsh=cm5zMXprZmphczAz" class="text-light text-decoration-none">
-                            <i class="bi bi-instagram me-1"></i> Instagram
-                        </a>
-                    </p>
-                    <p class="mb-1 small">
-                        <a href="https://www.facebook.com/share/19kUWTvjNF/?mibextid=wwXIfr" class="text-light text-decoration-none">
-                            <i class="bi bi-facebook me-1"></i> Facebook
-                        </a>
-                    </p>
-                    <p class="mb-1 small">
-                        <a href="https://wa.me/50612345678" class="text-light text-decoration-none" target="_blank">
-                            <i class="bi bi-whatsapp"></i> WhatsApp
-                        </a>
-                    </p>
-                </div>
-                <div class="col-md-3 mb-2">
-                    <h6 class="fw-bold text-uppercase">Oficina Central</h6>
-                    <p class="mb-1 small">Avenida 1A, Cartago Province, Cartago</p>
-                    <h6 class="fw-bold mt-2 small">Contacto</h6>
-                    <p class="mb-1 small">8813-9883 || 2592-5460</p>
-                    <p class="mb-0 small">opticagrisol@gmail.com</p>
-                </div>
+    <div class="container text-center">
+        <div class="row justify-content-center">
+            <div class="col-md-3 mb-2">
+                <h6 class="fw-bold text-uppercase">Sucursal</h6>
+                <a href="https://maps.app.goo.gl/8xCe7rQRBhBzRZsr7" class="text-light text-decoration-none small">Ver ubicación</a>
             </div>
-
-            <hr class="border-secondary">
-
-            <div class="row">
-                <div class="col">
-                    <p class="mb-0 small">
-                        &copy; <script>document.write(new Date().getFullYear());</script> Óptica Grisol. Todos los derechos reservados.
-                    </p>
-                </div>
+            <div class="col-md-3 mb-2">
+                <h6 class="fw-bold text-uppercase">Redes Sociales</h6>
+                <p class="mb-1 small">
+                    <a href="https://www.instagram.com/opticagrisol" class="text-light text-decoration-none">
+                        <i class="bi bi-instagram me-1"></i> Instagram
+                    </a>
+                </p>
+                <p class="mb-1 small">
+                    <a href="https://www.facebook.com/share/19kUWTvjNF/" class="text-light text-decoration-none">
+                        <i class="bi bi-facebook me-1"></i> Facebook
+                    </a>
+                </p>
+                <p class="mb-1 small">
+                    <a href="https://wa.me/50612345678" class="text-light text-decoration-none" target="_blank">
+                        <i class="bi bi-whatsapp"></i> WhatsApp
+                    </a>
+                </p>
+            </div>
+            <div class="col-md-3 mb-2">
+                <h6 class="fw-bold text-uppercase">Oficina Central</h6>
+                <p class="mb-1 small">Avenida 1A, Cartago Province, Cartago</p>
+                <h6 class="fw-bold mt-2 small">Contacto</h6>
+                <p class="mb-1 small">8813-9883 || 2592-5460</p>
+                <p class="mb-0 small">opticagrisol@gmail.com</p>
             </div>
         </div>
-    </footer>';
+
+        <hr class="border-secondary">
+
+        <div class="row">
+            <div class="col">
+                <p class="mb-0 small">
+                    &copy; <script>document.write(new Date().getFullYear());</script> Óptica Grisol. Todos los derechos reservados.
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>';
 }
 
 function IncluirCSS() {
@@ -138,30 +141,29 @@ function IncluirCSS() {
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> 
-    <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.min.css">
-    
-    <link rel="stylesheet" href="assets/vendor/glightbox/css/glightbox.min.css">
-    <link rel="stylesheet" href="assets/vendor/swiper/swiper-bundle.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css?v=5.8">
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+
+    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="/assets/vendor/bootstrap-icons/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/assets/vendor/glightbox/css/glightbox.min.css">
+    <link rel="stylesheet" href="/assets/vendor/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="/assets/css/styles.css?v=5.8">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
     ';
 }
 
 function IncluirScripts() {
     echo '
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/aos/aos.js"></script>
-    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/vendor/aos/aos.js"></script>
+    <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="/assets/vendor/php-email-form/validate.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="assets/js/registro.js"></script>
-    <script src="assets/js/scripts.js"></script>
-   
-  
+    <script src="/assets/js/registro.js"></script>
+    <script src="/assets/js/scripts.js"></script>
     ';
 }
+
 ?>
